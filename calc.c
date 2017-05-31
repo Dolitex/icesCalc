ee7ca13099a7ed7a9d1126bc00facc7e87544c4d
Rian Reis <contato@rianreis.com.br>
2017-05-31 02:45:49 -0300

    Base da calculadora criada e adição do arquivo ReadMe

---
+++ calc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	char comando;
+	int n1, n2, result;
+
+	printf("Digite o sinal da operação desejada.\n");
+	scanf("%c", &comando);
+
+	printf("Digite os dois números da operação.\n");
+	scanf("%d %d", &n1, &n2);
+
+
+
+
+
+	printf("O resultado é: %d", result);
+
+
+	return 0;
+}
